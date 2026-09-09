789f02d59fb7d41efa41bc6bb81dca0d0b65a24b
Levi-Mid <abdomlevi1999@gmail.com>
2026-09-09 12:48:09 -0300

    alterações iniciais na SideBar
    adicionando um pouco de cor, imagens e funções para determinadas ativações

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,8 +1,28 @@
 import "./Sidebar.css";
+import logoImg from "../../img/mid.png"
 
-function Sidebar() {
-    return( 
-    <h1>Sidebar teste</h1>
+function Sidebar({ activeItem, setActiveItem }) {
+
+    const navItems = [
+        { id: 1, label: "Home" },
+        { id: 2, label: "Novo Colaborador" },
+        { id: 3, label: "Funcionários" },
+        { id: 4, label: "Equipamentos" }
+    ]
+    return (
+        <aside className="sidebar">
+            <div className="logo-container">
+                <img src={logoImg} className="logo" alt="logo" />
+            </div>
+            <ul className="nav-list">
+                {navItems.map(item => (
+                    <li key={item.id} className={item.id === activeItem ? "active" : ""}>
+                        <button className="btn btn-basic" onClick={() => setActiveItem(item.id)}>
+                            {item.label}
+                        </button>
+                    </li>))}
+            </ul>
+        </aside>
     )
 }
 
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,15 +1,19 @@
 import "./Dashboard.css";
 import Sidebar from "../../components/Sidebar/Sidebar.jsx"
 import Topbar from "../../components/Topbar/Topbar.jsx"
+import { useState } from "react";
 
 function Dashboard(){
+    const[activeItem, setActiveItem] = useState(1)
     return(
+        
         <>
         <div className="dashboard">
-            <h1>Dashboard</h1>
+            <Sidebar activeItem={activeItem} setActiveItem={setActiveItem} />
+            <main className ="content-area">
+                <Topbar/>
+            </main>
         </div>
-        <Sidebar/>
-        <Topbar/>
         </>
     )
 }
