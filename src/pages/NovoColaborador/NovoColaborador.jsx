afe5e8e8d7de4985de83553dbefa82708f941c4b
Levi-Mid <abdomlevi1999@gmail.com>
2026-09-17 07:37:02 -0300

    adição ao SupaBase
    conexão com o banco de dados e testes para salvar as informações do funcionários, tudo ocorrendo corretamente.
    ps: esqueci de dar commit ontem :(

--- src/pages/NovoColaborador/NovoColaborador.jsx
+++ src/pages/NovoColaborador/NovoColaborador.jsx
@@ -1,17 +1,23 @@
 import "./NovoColaborador.css"
 import { useState } from "react";
+import {supabase} from "../../services/supabaseClient"
+
+const colaboradorVazio={
+    nome: "",
+    cargo: "",
+    endereco: "",
+    telefone: "",
+    email: "",
+    documento:""
+}
 
 function NovoColaborador(){
 
-    const [colaborador, setColaborador] = useState({
-        nome: "",
-        cargo: "",
-        endereco: "",
-        telefone: "",
-        email: "",
-        documento:""
-    })
+    const [colaborador, setColaborador] = useState(colaboradorVazio)
 
+    function limparDados(){
+        setColaborador(colaboradorVazio)
+    }
     function mudancaInput(e){
         setColaborador({...colaborador, [e.target.name]: e.target.value})
     }
@@ -20,14 +26,29 @@ function NovoColaborador(){
         value: colaborador[key]
     }));
 
+    async function salvarColaborador(){
+        const {data, error} = await supabase
+        .from('colaboradores')
+        .insert([colaborador])
+        .select()
+        if (error) {
+            alert(`Erro ao salvar colaborador: ${error.message}`);
+        }
+        else{
+            alert('Colaborador salvo com sucesso!');
+            limparDados();
+        }
+    }
+
     return(
         <div className="novo-colaborador">
             {pessoas.map(pessoa =>(
-                <label>
+                <label key={pessoa.name}>
                     {pessoa.name}:
                     <input type="text" className="form-control" name={pessoa.name} value={pessoa.value} onChange={mudancaInput} />
                 </label>
             ))} 
+            <button type="button" onClick={salvarColaborador}>SALVAR</button>
         </div>
     )
 }
