106b6b8c038c354af46253668847832d22fa8f81
Levi-Mid <abdomlevi1999@gmail.com>
2026-09-17 12:47:58 -0300

    pequeno update
    mantive apenas o inicio, n foi nada finalizado

--- src/pages/Funcionarios/Funcionarios.jsx
+++ src/pages/Funcionarios/Funcionarios.jsx
@@ -1,6 +1,16 @@
 import "./Funcionarios.css";
+import { useState } from "react";
+import { useEffect } from "react";
+import { supabase } from "../../services/supabaseClient";
+
 
 function Funcionarios(){
+    const[funcionarios, setFuncionario] = useState([])
+    useEffect = {
+        async function buscarDados() {
+            
+        }
+    }
     return(
         <div>
             <h1> Teste de Funcionários</h1>
