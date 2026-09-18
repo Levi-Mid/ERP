87b1e462467560e99776a30ab1a49fb503ac89d9
Levi-Mid <abdomlevi1999@gmail.com>
2026-09-18 12:33:16 -0300

    integração entre paginas
    Dados de usuários sendo finalizado e passando para outra área da pagina.

--- src/pages/Funcionarios/Funcionarios.jsx
+++ src/pages/Funcionarios/Funcionarios.jsx
@@ -4,18 +4,36 @@ import { useEffect } from "react";
 import { supabase } from "../../services/supabaseClient";
 
 
-function Funcionarios(){
-    const[funcionarios, setFuncionario] = useState([])
-    useEffect = {
+function Funcionarios() {
+    const [funcionarios, setFuncionario] = useState([])
+    useEffect(() => {
         async function buscarDados() {
-            
+            const {data, error} = await supabase
+                .from('colaboradores')
+                .select('*')
+                .eq('status','completo')
+            if(error) console.log(error.message)
+            else{
+                setFuncionario(data)
+            }
         }
-    }
-    return(
-        <div>
-            <h1> Teste de Funcionários</h1>
+        buscarDados()
+    }, [])
+
+
+    return (
+        <div className="allCards">
+            {funcionarios.map(func => (
+                <div className="cards completos" key={func.id}>
+                    <p className="nome">{func.nome}</p>
+                    <p className="cargo">{func.cargo}</p>
+                    <p className="tel">{func.telefone}</p>
+                    <p className="email">{func.email}</p>
+                </div>
+            ))}
         </div>
     )
 }
+
 
 export default Funcionarios;
--- src/pages/NovoColaborador/NovoColaborador.jsx
+++ src/pages/NovoColaborador/NovoColaborador.jsx
@@ -1,6 +1,7 @@
 import "./NovoColaborador.css"
 import { useState } from "react";
 import {supabase} from "../../services/supabaseClient"
+import { useEffect } from "react";
 
 const colaboradorVazio={
     nome: "",
@@ -40,16 +41,54 @@ function NovoColaborador(){
         }
     }
 
+    const [funPendentes, setFunPendente] = useState([])
+        useEffect(() => {
+            async function buscarDados() {
+                const {data, error} = await supabase
+                    .from('colaboradores')
+                    .select('*')
+                    .eq('status','pendente')
+                if(error) console.log(error.message)
+                else{
+                    setFunPendente(data)
+                }
+            }
+            buscarDados()
+        }, [])
+        async function completarCadastro(id){
+            const {data, error} = await supabase
+                .from('colaboradores')
+                .update({ status: 'completo' })
+                .eq('id', id)
+            if(error) console.log(error.message)
+            else {
+                alert("Cadastro finalizado com sucesso!")
+            }
+}
+        
     return(
-        <div className="novo-colaborador">
-            {pessoas.map(pessoa =>(
-                <label key={pessoa.name}>
-                    {pessoa.name}:
-                    <input type="text" className="form-control" name={pessoa.name} value={pessoa.value} onChange={mudancaInput} />
-                </label>
-            ))} 
-            <button type="button" onClick={salvarColaborador}>SALVAR</button>
-        </div>
+        <>
+            <div className="novo-colaborador">
+                {pessoas.map(pessoa =>(
+                    <label key={pessoa.name}>
+                        {pessoa.name}:
+                        <input type="text" className="form-control" name={pessoa.name} value={pessoa.value} onChange={mudancaInput} />
+                    </label>
+                ))} 
+                <button type="button" onClick={salvarColaborador}>SALVAR</button>
+            </div>
+            <div className="allCards">
+                {funPendentes.map(pendente => (
+                    <div className="cards pendentes" key={pendente.id}>
+                        <p className="nome">{pendente.nome}</p>
+                        <p className="cargo">{pendente.cargo}</p>
+                        <p className="tel">{pendente.telefone}</p>
+                        <p className="email">{pendente.email}</p>
+                        <button type="button" onClick={() => completarCadastro(pendente.id)}>Finalizar Cadastro</button>
+                    </div>
+                ))}
+            </div>
+        </>
     )
 }
 
